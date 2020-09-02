bc3aa7c154abf15e903e95188f068c67f7190493
Igor Gavilon <igorgavilon@gmail.com>
2020-09-02 10:25:33 -0400

    cria o novo projeto

---
+++ nossaCalculadora/nossaCalculadora/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  nossaCalculadora
+//
+//  Created by Igor Gavilon on 02/09/20.
+//  Copyright © 2020 igorGavilon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
